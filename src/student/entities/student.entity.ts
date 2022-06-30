67cfdb2f25a236e8ca749cd70e491a21a4b2766c
Achnit Reda <achnitreda@gmail.com>
2022-06-30 18:36:32 +0100

    create my modules and there entities

--- src/student/entities/student.entity.ts
+++ src/student/entities/student.entity.ts
@@ -1,8 +1,20 @@
+import { Course } from 'src/course/entities/course.entity';
+import {Column,Entity,JoinTable,ManyToMany,PrimaryGeneratedColumn,} from 'typeorm';
+@Entity()
 export class Student {
+  @PrimaryGeneratedColumn()
   id: number;
+  @Column()
   first_name: string;
+  @Column()
   last_name: string;
+  @Column()
   age: number;
+  @Column()
   cni: string;
+  @Column()
   cne: string;
+  @ManyToMany(() => Course, (course) => course.students)
+  @JoinTable()
+  courses: Course[];
 }
